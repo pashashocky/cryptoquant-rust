9487708490062879191b6585deea267d48aa69aa
Pash Shocky <pashashocky@gmail.com>
2024-08-22 21:07:16 +0100

    feat(db): first indexing into db

--- src/data/binance/mod.rs
+++ src/data/binance/mod.rs
@@ -1,6 +1,6 @@
 pub mod data_types;
 pub mod downloader;
-mod file;
+pub mod file;
 mod file_collection;
 mod pair;
 mod s3;
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,22 @@ pub mod data;
 pub mod utils;
 pub use crate::data::binance::data_types::{Asset, Cadence, DataType};
 pub use crate::data::binance::downloader::Downloader;
+// pub use crate::data::binance::file::Row;
+pub use crate::data::db::Table;
+// use crate::utils::config;
 
 use std::env;
+// use std::path::PathBuf;
 use std::time::Instant;
 
 use anyhow::Result;
+// use anyhow::Context;
+// use async_zip::base::read::seek::ZipFileReader;
 use env_logger::{Builder, Target};
-use tokio::sync::mpsc;
+// use tokio::fs::File;
+// use tokio::io::BufReader;
+// use tokio_stream::StreamExt;
+// use tokio_util::compat::FuturesAsyncReadCompatExt;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -17,30 +26,21 @@ async fn main() -> Result<()> {
         .parse_filters(&env::var("RUST_LOG").unwrap_or_else(|_| "info".to_string()))
         .init();
 
+    // perf start
     let now = Instant::now();
-    let mut bh = Downloader::new(
+
+    let downloader = Downloader::new(
         "USDC Downloader",
         Asset::Spot,
         Cadence::Monthly,
         DataType::Trades,
     )?
-    .with_pair_ends_with(&["USDC"]);
+    .with_pair_ends_with(&["ZKUSDC"]);
 
-    let (tx, mut rx) = mpsc::channel(10);
-    tokio::spawn(async move {
-        bh.download_with_channel(Some(tx)).await?;
-        Ok::<_, anyhow::Error>(())
-    });
+    let table = Table::new("test", "trades_any_usdc", downloader).await?;
+    table.index().await?;
 
-    let mut i = 0;
-    while let Some(path) = rx.recv().await {
-        log::debug!("Received path={}", path.to_string_lossy());
-        i += 1;
-    }
+    log::info!("Took: {:.2?}", now.elapsed());
 
-    let elapsed = now.elapsed();
-
-    log::info!("Took: {:.2?}", elapsed);
-    log::info!("Received {}", i);
     Ok(())
 }
